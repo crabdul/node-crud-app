1df2f8f1c37e21b693724f097bdd451fb523c7c9
Abdulkarim El Khazaani <abdulkarim.elkhazaani@octoenergy.com>
2018-08-16 13:59:52 +0100

    retrieving data from requests

--- index.js
+++ index.js
@@ -5,10 +5,46 @@
 // Dependencies
 
 const http = require('http');
+const url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond with a string
 const server = http.createServer((req, res) => {
-    res.end('Hello World\n');
+    // Get the URL and parse it
+    const parseURL = url.parse(req.url, true);
+
+    // Get the path
+    const path = parseURL.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Get the query string as an object
+    const queryStringObject = parseURL.query;
+
+    // Get the HTTP Method
+    const method = req.method.toLowerCase();
+
+    // Get the headers as an object
+    const headers = req.headers;
+
+    // Get the payload, if any
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    // As the data is streaming in, the req object emits a 'data' event.
+    // We subscribe to this event and we decode with utf-8 and write to the buffer
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
+
+    // 'end' event always emitted even if there is no payload
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        // Send the response
+        res.end('Hello World\n');
+        // Log the request path
+
+        console.log('Request received with this payload: ', buffer);
+    });
 });
 
 // Start the server, and have it listen on port 3000
